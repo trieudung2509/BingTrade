9de7345121ebeaccc81c90b0424d078c202abb25
@nguyenvankhoa <nguyenvankhoa@gapo.com.vn>
2022-06-17 14:53:02 +0700

    update index php file

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,22 @@
-<footer class="footer">
-	<div class="container">footer</div>
-</footer>
-<!-- /.footer -->
+ <!-- start footer block -->
+ <footer class="footer">
+    <div class="container">
+      <div class="main-footer">
+        <a href="#">
+          <img src="./images/logo.png" alt="footer-logo" class="footer-logo" />
+        </a>
+        <p class="footer-description">
+          BingTrade is an affiliate program promoting service packages of Bingsport - one of the leading platforms on
+          live sports services offering the greatest live coverage of over 1000 sports
+        </p>
+        <div class="contact">
+          <p class="website">Website: Bingsport.com</p>
+          <p class="email">Email: contact@bingtrade.net</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- /.footer -->
 
 <!-- JAVASCRIPT-->
 <script src="js/jquery-3.3.1.min.js"></script>
---
+++ templates/home-page-2.php
@@ -0,0 +1,337 @@
+ <!-- to do block -->
+ <div class="how-to-do">
+    <div class="container">
+      <div class="title-wrapper">
+        <h2 class="to-do-title"><span>How to do </span> <span class="dash"></span></h2>
+        <span class="dash"></span>
+      </div>
+      <div class="list-todo">
+        <div class="todo-item">
+          <div class="todo-title">
+            <span class="number">1</span>
+            <img src="./images/todo-arrow.png" alt="" class="arrow">
+          </div>
+          <div class="todo-content">
+            <h3 class="label">Posting</h3>
+            <p class="description">Put the link of BingTrade service packages in your social media posts</p>
+            <ul class="list-contact">
+              <li class="contact-item">
+                <a href="">
+                  <img src="./images/twitter-ic.png" alt="">
+                </a>
+              </li>
+              <li class="contact-item">
+                <a href="">
+                  <img src="./images/facebook-icpng.png" alt="">
+                </a>
+              </li>
+              <li class="contact-item">
+                <a href="">
+                  <img src="./images/instagram-ic.png" alt="">
+                </a>
+              </li>
+              <li class="contact-item">
+                <a href="">
+                  <img src="./images/youtube-ic.png" alt="">
+                </a>
+              </li>
+              <li class="contact-item">
+                <a href="">
+                  <img src="./images/telegram-ic.png" alt="">
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="todo-item">
+          <div class="todo-title">
+            <span class="number">2</span>
+            <img src="./images/todo-arrow.png" alt="" class="arrow">
+          </div>
+          <div class="todo-content">
+            <h3 class="label">Get interested from customers</h3>
+            <p class="description">Users who click on the link will be redirected directly to BingTrade's website</p>
+          </div>
+        </div>
+        <div class="todo-item">
+          <div class="todo-title">
+            <span class="number">3</span>
+            <img src="./images/todo-arrow.png" alt="" class="arrow">
+          </div>
+          <div class="todo-content">
+            <h3 class="label">Conversion</h3>
+            <p class="description">Users make purchase and pay for service packs within 30 days after clicking on the
+              referral link</p>
+          </div>
+        </div>
+        <div class="todo-item">
+          <div class="todo-title">
+            <span class="number">4</span>
+            <img src="./images/todo-arrow.png" alt="" class="arrow">
+          </div>
+          <div class="todo-content">
+            <h3 class="label">Get commission</h3>
+            <p class="description">BingTrade confirms and pays for the conversion, you get a commission</p>
+          </div>
+        </div>
+      </div>
+      <div class="todo-control">
+        <button>Register Now</button>
+      </div>
+    </div>
+  </div>
+
+  <!-- end to do block -->
+
+  <!-- start register block -->
+  <div class="register-block">
+    <div class="container">
+      <div class="title-wrapper">
+        <h2 class="register-title"><span>Register to be a BingTrade's Partner today.</span> <span class="dash"></span></h2>
+      </div>
+      <p class="register-description">Fill out the information below and click sign up button to complete</p>
+      <from class="register-form">
+        <!-- start line 1 -->
+        <div class="form-line">
+          <div class="form-item">
+            <label>
+              <span>Full Name</span>
+              <span class="required">(*)</span>
+              <br />
+              <input type="text" value="" name="" placeholder="Full name" />
+            </label>
+          </div>
+          <div class="form-item">
+            <label>
+              <span>Company Website</span>
+              <br />
+              <input type="text" value="" name="" placeholder="www.yourdomain.com" />
+            </label>
+          </div>
+        </div>
+        <!-- end line 1 -->
+
+        <!-- start line 2 -->
+        <div class="form-line">
+          <div class="form-item">
+            <label>
+              <span>Full Name</span>
+              <span class="required">(*)</span>
+              <br />
+              <input type="text" value="" name="" placeholder="Full name" />
+            </label>
+          </div>
+          <div class="form-item">
+            <label>
+              <span>Company Website</span>
+              <br />
+              <input type="text" value="" name="" placeholder="www.yourdomain.com" />
+            </label>
+          </div>
+        </div>
+        <!-- end line 2-->
+
+        <!-- start line 3 -->
+        <div class="form-line">
+          <div class="form-item">
+            <label>
+              <span>Full Name</span>
+              <span class="required">(*)</span>
+              <br />
+              <input type="text" value="" name="" placeholder="Full name" />
+            </label>
+          </div>
+          <div class="form-item">
+            <label>
+              <span>Company Website</span>
+              <br />
+              <input type="text" value="" name="" placeholder="www.yourdomain.com" />
+            </label>
+          </div>
+        </div>
+        <!-- end line 3 -->
+
+        <!-- start line 4 -->
+        <div class="form-line">
+          <div class="form-item">
+            <label>
+              <span>Full Name</span>
+              <span class="required">(*)</span>
+              <br />
+              <input type="text" value="" name="" placeholder="Full name" />
+            </label>
+          </div>
+          <div class="form-item">
+            <label>
+              <span>Company Website</span>
+              <br />
+              <input type="text" value="" name="" placeholder="www.yourdomain.com" />
+            </label>
+          </div>
+        </div>
+        <!-- end line 4 -->
+
+        <!-- start line 5-->
+        <div class="form-line">
+          <div class="form-item">
+            <label>
+              <span>Full Name</span>
+              <span class="required">(*)</span>
+              <br />
+              <input type="text" value="" name="" placeholder="Full name" />
+            </label>
+          </div>
+          <div class="form-item">
+            <label>
+              <span>Company Website</span>
+              <br />
+              <input type="text" value="" name="" placeholder="www.yourdomain.com" />
+            </label>
+          </div>
+          <!-- end line 5 -->
+        </div>
+        <!-- promotional block -->
+        <div class="promotion-block">
+          <div class="title-wrapper">
+            <h2 class="register-title"><span>Promotional Information </span> <span class="dash"></span></h2>
+          </div>
+          <div class="form-line">
+            <div class="form-item">
+              <label>
+                <span>Partner Type</span>
+                <span class="required">(*)</span>
+                <br />
+                <input type="text" value="" name="" placeholder="Full name" />
+              </label>
+            </div>
+          </div>
+          <button class="btn-sign-up">Sign up</button>
+        </div>
+        <!-- end promotional block -->
+        </form>
+    </div>
+  </div>
+  <!-- ends register block -->
+
+  <!-- start faq block -->
+  <div class="faq-block">
+    <div class="container">
+      <div class="title-wrapper">
+        <h2 class="faq-title">FAQs</h2>
+        <span class="dash"></span>
+      </div>
+      <div class="faq-wrapper">
+        <div class="faq-content">
+          <div class="list-problem">
+            <div class="problem-item">
+              <button type="button" class="collapsible">
+                <div class="collapse-title">
+                  <span class="number">1</span>
+                  <h3 class="title">Is it okay if I have no experience in teaching english before? </h3>
+                </div>
+                <div class="collapse-icon">
+                  <img src="images/icon-arrow-up.png" alt="" class="icon-arrow icon-arrow-up">
+                  <img src="images/icon-arrow-down.png" alt="" class="icon-arrow icon-arrow-down">
+                </div>
+              </button>
+              <div class="problem-content">
+                <p class="content-detail">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis accusamus praesentium velit fuga
+                  numquam porro. Est voluptatem, agni eum facilis sapiente modi.
+                </p>
+              </div>
+            </div>
+            <div class="problem-item">
+              <button type="button" class="collapsible">
+                <div class="collapse-title">
+                  <span class="number">2</span>
+                  <h3 class="title">What should I prepare in advance for each class? </h3>
+                </div>
+                <div class="collapse-icon">
+                  <img src="images/icon-arrow-up.png" alt="" class="icon-arrow icon-arrow-up">
+                  <img src="images/icon-arrow-down.png" alt="" class="icon-arrow icon-arrow-down">
+                </div>
+              </button>
+              <div class="problem-content">
+                <p class="content-detail">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati perspiciatis eaque assumenda enim!
+                  Modi natus mollitia nemo, maioreseaque?
+                </p>
+              </div>
+            </div>
+            <div class="problem-item">
+              <button type="button" class="collapsible">
+                <div class="collapse-title">
+                  <span class="number">3</span>
+                  <h3 class="title">Is it okay if I have no experience in teaching english before? </h3>
+                </div>
+                <div class="collapse-icon">
+                  <img src="images/icon-arrow-up.png" alt="" class="icon-arrow icon-arrow-up">
+                  <img src="images/icon-arrow-down.png" alt="" class="icon-arrow icon-arrow-down">
+                </div>
+              </button>
+              <div class="problem-content">
+                <p class="content-detail">Lorem ipsum dolor sit amet conseus aliquid modi excepturi quibusdam.
+                </p>
+              </div>
+            </div>
+            <div class="problem-item">
+              <button type="button" class="collapsible">
+                <div class="collapse-title">
+                  <span class="number">4</span>
+                  <h3 class="title">Will I get a bonus if I perform excellently in a month? </h3>
+                </div>
+                <div class="collapse-icon">
+                  <img src="images/icon-arrow-up.png" alt="" class="icon-arrow icon-arrow-up">
+                  <img src="images/icon-arrow-down.png" alt="" class="icon-arrow icon-arrow-down">
+                </div>
+              </button>
+              <div class="problem-content">
+                <p class="content-detail">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima vel ratione
+                  ipsa magnam! Fugiat eligendi rem possimus laudantium iste iure distinctd.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <img src="./images/faq-image.png" alt="" class="image-faq">
+      </div>
+    </div>
+  </div>
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/slick.min.js"></script>
+  <script src="js/main.js"></script>
+  <script>
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        const isActive = this.classList.contains('active')
+        const iconArrowUp = this.querySelector('.icon-arrow-up')
+        const iconArrowDown = this.querySelector('.icon-arrow-down')
+        if (isActive) {
+          iconArrowDown.style.display = 'none'
+          iconArrowUp.style.display = 'block'
+        } else {
+          iconArrowDown.style.display = 'block'
+          iconArrowUp.style.display = 'none'
+        }
+        if (content.style.maxHeight) {
+          content.style.maxHeight = null;
+        } else {
+          content.style.maxHeight = content.scrollHeight + "px";
+        }
+      });
+    }
+
+    function handleClick(item) {
+      const id = item.getAttribute('id')
+      const element = document.getElementById(`${id}-content`)
+      window.scrollTo(0, element.offsetTop - item.offsetHeight);
+    }
+  </script>
