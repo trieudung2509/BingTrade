504b2864462add3401beebfc3979a1a54b9cff0d
Lucas Mario <lucas.brightsoft@gmail.com>
2022-06-21 08:41:05 +0700

    fix: ui modal

--- index.php
+++ index.php
@@ -208,21 +208,20 @@
 	</div>
 	<?php include "templates/home-page-2.php"; ?>
 	<?php include "templates/footer.php"; ?>
-</body><!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Thankyou</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-					Congratulations on your successful registration. Please check your email for further instructions on how to get started. Thank you
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
+	<!-- Modal -->
+	<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog modal-dialog-centered">
+			<div class="modal-content">
+				<div class="modal-header">
+						<img src="images/close.png" class="pull-right" data-bs-dismiss="modal" aria-label="Close" />
+				</div>
+				<div class="modal-body">
+					<img src="images/close-success.png" alt="">
+					<h5>Congratulations on your successful registration</h5>
+					<p>Please check your email for further instructions on how to get started. Thank you</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
 </html>
