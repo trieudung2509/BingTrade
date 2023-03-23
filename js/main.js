7b7abe7bf01c4c11537ffb2396f90fba2c750398
DangKhoa96 <khoanguyenptit96@gmail.com>
2023-03-24 02:28:42 +0700

    create html file

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
 $(function() {
+	/* -----------------------------------------------------------
+		Header fixed
+	----------------------------------------------------------- */
+	$(window).on('scroll', function() {
+		if ($(window).scrollTop() > 0) {
+			$('body').addClass('fixed-header').css('padding-top', $('.main-header').outerHeight());
+		} else {
+			$('body').removeClass('fixed-header').css('padding-top', '');
+		}
+	});
 });
--- templates/header.php
+++ templates/header.php
@@ -3,19 +3,45 @@
 		<div class="row g-0 justify-content-between align-items-center">
 			<div class="col-auto col-logo">
 				<a href="#" class="logo">
-					<img src="images/logo.png" alt="Bing trade" class="logo">
+					<img src="./images/logo.png" alt="yeahscore" class="d-lg-flex">
 				</a>
 			</div>
 			<div class="col-auto col-nav">
-				<ul class="main-nav">
-					<li><a href="#">About Us</a></li>
-					<li><a href="#">Program</a></li>
-					<li><a href="#">How to do</a></li>
-					<li><a href="#">FAQs</a></li>
-					<li><a href="#">Register</a></li>
+				<ul class="main-nav d-none d-lg-flex">
+					<li><a href="#">SPORTS</a></li>
+					<li><a href="#">Package</a></li>
+					<li><a href="#">Highlight</a></li>
+					<li class="user-avatar">
+						<img src="./images/user-avatar-default.png" alt="">
+					</li>
 				</ul>
+				<button class="btn border-0 d-block d-lg-none btn-menu" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasMenu" aria-controls="offcanvasMenu">
+					<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M1.42871 4.42871H18.5716M1.42871 10.143H18.5716M1.42871 15.8573H18.5716" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+					</svg>
+				</button>
 			</div>
 		</div>
 	</div>
 </header>
+<!-- /END .main-header -->
+<div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasMenu">
+	<div class="offcanvas-header">
+		<h5 class="offcanvas-title">
+			<a href="#" class="logo"><img src="./images/logo.png" alt="yeahscore"></a>
+		</h5>
+		<button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+	</div>
+	<div class="offcanvas-body">
+		<ul class="main-nav">
+			<li><a href="#">SPORTS</a></li>
+					<li><a href="#">Package</a></li>
+					<li><a href="#">Highlight</a></li>
+					<li class="user-avatar">
+						<img src="./images/user-avatar-default.png" alt="">
+					</li>
+		</ul>
+	</div>
+</div>
+<!-- /END .offcanvas -->
 <!-- /.main-header -->
